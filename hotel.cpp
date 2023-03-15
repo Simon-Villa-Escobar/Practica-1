d18986866d9e16a46073ce681d969531fa5e1a9c
Simon-Villa-Escobar <123431231+Simon-Villa-Escobar@users.noreply.github.com>
2023-03-15 15:50:08 -0500

    Add files via upload

---
+++ hotel.cpp
@@ -0,0 +1,198 @@
+#include "hotel.h"
+
+ListaHuespedes::ListaHuespedes() {
+    primero = NULL;
+    ultimo = NULL;
+}
+/*
+ListaHuespedes::~ListaHuespedes() {
+    Huesped* actual = primero;
+    while (actual != NULL) {
+        Huesped* siguiente = actual->siguiente;
+        delete actual;
+        actual = siguiente;
+    }
+}*/
+
+void ListaHuespedes::cargarHuesped(string nombre, int habitacion) {
+    Huesped* nuevoHuesped = new Huesped();
+    nuevoHuesped->nombre = nombre;
+    nuevoHuesped->habitacion = habitacion;
+    nuevoHuesped->siguiente = NULL;
+
+    if (primero == NULL) {
+        nuevoHuesped->anterior = NULL;
+        primero = nuevoHuesped;
+        ultimo = nuevoHuesped;
+    }
+    else {
+        nuevoHuesped->anterior = ultimo;
+        ultimo->siguiente = nuevoHuesped;
+        ultimo = nuevoHuesped;
+    }
+}
+
+void ListaHuespedes::imprimirHuespedes() {
+    Huesped* actual = primero;
+    while (actual != NULL) {
+        cout << "Nombre: " << actual->nombre << ", Habitación: " << actual->habitacion << endl;
+        actual = actual->siguiente;
+    }
+}
+
+void ListaHuespedes::agregarHuesped(string nombre, int habitacion){
+  Huesped* nuevoHuesped = new Huesped();
+  nuevoHuesped->nombre = nombre;
+  nuevoHuesped->habitacion = habitacion;
+  nuevoHuesped->siguiente = NULL;
+  nuevoHuesped->anterior = ultimo;
+  ultimo->siguiente = nuevoHuesped;
+  ultimo = nuevoHuesped;
+
+  //Agregando el nuevo huesped en huespedes.txt
+  ofstream archivo("huespedes.txt", ios::app);
+    archivo << nombre << "\n" << habitacion<<"\n";
+    archivo.close();
+  
+}
+
+void ListaHuespedes::buscarHuespedNombre(string nombre){
+  Huesped* actual = primero;
+  while(actual != NULL){
+    if (actual->nombre ==  nombre){
+      cout<<nombre<<" está en la habitación: "<<actual->habitacion<<endl;
+    }
+    actual = actual->siguiente;
+  }
+}
+
+void ListaHuespedes::buscarHuespedHabitacion(int habitacion){
+  Huesped* actual = primero;
+  while(actual!= NULL){
+    if (actual->habitacion == habitacion){
+      cout<<"El huesped que está en la habitación "<<habitacion<<" es: "<<actual->nombre<<endl;
+    }
+    actual = actual->siguiente;
+  }
+}
+
+void ListaHuespedes::ordenarLista() {
+    Huesped* sortedHead = NULL;
+    Huesped* current = primero;
+    while (current != NULL) {
+        Huesped* newNode = new Huesped();
+        newNode->nombre = current->nombre;
+        newNode->habitacion = current->habitacion; // Asignar el número de habitación
+        newNode->anterior = NULL;
+        newNode->siguiente = NULL;
+        if (sortedHead == NULL) {
+            sortedHead = newNode;
+        } else {
+            Huesped* temp = sortedHead;
+            while (temp != NULL) {
+                if (newNode->nombre < temp->nombre) {
+                    if (temp->anterior != NULL) {
+                        temp->anterior->siguiente = newNode;
+                        newNode->anterior = temp->anterior;
+                    } else {
+                        sortedHead = newNode;
+                    }
+                    temp->anterior = newNode;
+                    newNode->siguiente = temp;
+                    break;
+                } else if (temp->siguiente == NULL) {
+                    temp->siguiente = newNode;
+                    newNode->anterior = temp;
+                    break;
+                }
+                temp = temp->siguiente;
+            }
+        }
+        current = current->siguiente;
+    }
+    Huesped* temp = sortedHead;
+    while (temp != NULL) {
+        cout << "Nombre: " << temp->nombre << ", Habitación: " << temp->habitacion << endl;
+        temp = temp->siguiente;
+    }
+}
+
+/*Roberto
+7
+Luis
+12, esto es un archivo .txt, con este archivo cree una lista doblemente enlazada que guarda el nombre y la habitacion de cada huesped que se llama listaHuespedes. Necesito crear otra lista en una funcion que no sea el main, y esta lista tiene los huespedes en orden alfabetico, y imprime la lista diciendo el nombre de cada huesped y su habitacion, en c++*/
+
+
+
+
+
+
+void ListaHuespedes::consultarVecinos(int habitacion){
+  int ant = habitacion -1;
+  int sig = habitacion + 1;
+  Huesped* actual  = primero;
+  bool a = true;
+  bool b = true;
+  while(actual != NULL){
+    if(actual->habitacion == ant){
+      cout<<"El vecino en la habitacion de antes es: "<<actual->nombre<<endl;
+      a = false;
+    }
+    if(actual->habitacion == sig){
+      cout<<"El vecino en la habitacion siguiente es: "<<actual->nombre<<endl;
+      b = false;
+    }
+    actual = actual-> siguiente;
+  }
+  if(a == true){
+    cout<<"No hay vecino en la habitacion de antes..."<<endl;
+  }
+  if(b == true){
+    cout<<"No hay vecino en la habitacion siguiente..."<<endl;
+  }
+
+  
+}
+  
+
+
+int ListaHuespedes::consultarHabitacion(int habitacion){
+  int a = 0;
+  bool b = false;
+  bool c = false;
+  int ant = habitacion - 1;
+  int sig = habitacion + 1;
+  Huesped* actual = primero;
+  Huesped* ante = primero;
+  Huesped* siguien = primero;
+  while(actual!=NULL){
+    if(actual->habitacion == habitacion){
+      a = 4;
+      while(ante!=NULL){
+        if(ante->habitacion == ant){
+          a = 1;
+          b = true;
+        }
+        ante = ante->siguiente;
+      }
+      while(siguien!=NULL){
+        if(siguien->habitacion == sig){
+          a = 2;
+          c = true;
+        }
+        siguien = siguien->siguiente;
+      }
+    }
+    if(b==true && c == true){
+      a = 3;
+    }
+    actual = actual->siguiente;
+  }
+  return a;
+  /* a=0, el numero de habitacion esta disponible,
+     a=1, la habitacion escogida y la anterior estan ocupadas
+     a=2, la habitacion escogida y la siguiente estan ocupadas
+     a=3, la habitacion escogida, la anterior y la siguiente estan ocupadas
+     a=4, la habitacion escogida esta ocupada
+  */
+}
---
+++ main.cpp
@@ -0,0 +1,152 @@
+
+//Practica 1 de Juan Diego Tamayo y Simon Villa Escobar
+
+#include "hotel.h"
+
+int main() {
+    ListaHuespedes listaHuespedes;
+
+    string nombre;
+    int habitacion;
+
+    /*ifstream archivoHuespedes("huespedes.txt");
+    while (archivoHuespedes >> nombre >> habitacion) {
+        listaHuespedes.cargarHuesped(nombre, habitacion);
+    }*/
+
+  ifstream archivoHuespedes("huespedes.txt");
+  //string nombre;
+  //int habitacion;
+  while (getline(archivoHuespedes, nombre) && archivoHuespedes >> habitacion) {
+    listaHuespedes.cargarHuesped(nombre, habitacion);
+    archivoHuespedes.ignore(); // ignorar el salto de línea después del número de habitación
+}
+
+
+    archivoHuespedes.close();
+  bool m = true;
+  int d;
+  while(m == true){
+    cout<<"\n";
+    cout<<"1. Agregar un huesped"<<endl;
+    cout<<"2. Buscar un huesped por el nombre"<<endl;
+    cout<<"3. Buscar un huesped por su habitacion"<<endl;
+    cout<<"4. Mostrar la lista de huespedes en orden alfabetico"<<endl;
+    cout<<"5. Mostrar la lista de huespedes en orden de llegada"<<endl;
+    cout<<"6. Consultar las vecinos"<<endl;
+    cout<<"7. Salir"<<endl;
+    cin>>d;
+
+    if(d == 1){    //Agregar un huesped
+      string n;
+      cout<<"Cual es tu nombre?"<<endl;
+      cin>>n;
+      bool dc = true;
+      while (dc == true){
+        int h;
+        cout<<"Que habitacion deseas?"<<endl;
+        cin>>h;
+        int ha;
+        ha = listaHuespedes.consultarHabitacion(h);
+        bool hab = true;
+        while (hab == true){
+          if(ha == 0){
+           
+            listaHuespedes.agregarHuesped(n, h);
+            hab = false;
+            dc = false;
+          }
+          if(ha==4){
+            int dh;
+            cout<<"La habitacion esta ocupada, quieres: 1.la habitacion anterior ("<<(h-1)<<"), 2.la habitacion siguiente ("<<(h+1)<<") o 3.deseas elegir otra habitacion?"<<endl;
+            cin>>dh;
+            if(dh == 1){
+              
+              listaHuespedes.agregarHuesped(n, (h-1));
+              hab = false;
+              dc = false;
+            }
+            if(dh == 2){
+              
+              listaHuespedes.agregarHuesped(n, (h+1));
+              hab = false;
+              dc = false;
+              
+            }
+            if(dh == 3){
+              hab = false;
+            }
+          }
+          if(ha == 1){
+            int dh;
+            cout<<"La habitacion esta ocupada, quieres: 1. La habitacion siguiente ("<<(h+1)<<") o 2.Quieres escoger otra habitacion?"<<endl;
+            cin>>dh;
+            if(dh==1){
+              listaHuespedes.agregarHuesped(n, (h+1));
+              hab = false;
+              dc = false;
+              
+            }
+            if(dh==2){
+              hab = false;
+            }
+          }
+          if(ha==2){
+            int dh;
+            cout<<"La habitacion esta ocupada, quieres: 1.La habitacion anterior("<<(h-1)<<") o 2.Quieres escoger otra habitacion?"<<endl;
+            cin>>dh;
+            if(dh==1){
+              listaHuespedes.agregarHuesped(n, (h-1));
+              hab = false;
+              dc = false;
+              
+            }
+            if (dh == 2){
+              hab = false;
+            }
+          }
+          if(ha == 3){
+            cout<<"La habitacion elegida, la anterior y la siguiente estan ocupadas. Elije otra habitacion"<<endl;
+            hab = false;
+          }
+        }
+      }
+    }
+
+    
+    if(d==2){    //Buscar un huesped por el nombre
+      string n;
+      cout<<"Como se llama el huesped que estas buscando?"<<endl;
+      cin>>n;
+      listaHuespedes.buscarHuespedNombre(n);
+      
+    }
+
+    if(d==3){    //Buscar un huesped por la habitacion
+      int h;
+      cout<<"En cual habitacion esta el huesped que estas buscando?"<<endl;
+      cin>>h;
+      listaHuespedes.buscarHuespedHabitacion(h);
+    }
+    if(d==4){    //Lista de los huespedes en orden alfabetico
+      listaHuespedes.ordenarLista();
+    }
+
+    if(d==5){    //Lista de los huespedes en orden de llegada
+      listaHuespedes.imprimirHuespedes();
+    }
+
+    if(d==6){    //Consultar los vecinos
+      int h;
+      cout<<"De cual habitacion deseas consultar los vecinos?"<<endl;
+      cin>>h;
+      listaHuespedes.consultarVecinos(h);
+    }
+
+    if(d==7){    //Salir del menu
+      cout<<"Hasta luego!"<<endl;
+      m = false;
+    }  
+  }
+    return 0;
+}
